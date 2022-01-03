5cf8022c35e55171082bf5136ebe280093e03355
Ashraf <ashrafweheliye25@gmail.com>
2022-01-03 14:10:11 +0000

    first commit

---
+++ src/components/OverlayMovie.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './OverlayMovie.css';
+export default function OverlayMovie({
+    title,
+    date,
+    overview,
+    clickedValue,
+    setClickedValue,
+    info,
+}) {
+    // useEffect(() => {
+    //     const handleClick = () => {
+    //         setClickedValue(false);
+    //     };
+    // }, [clickedValue]);
+    console.log('hello');
+
+    return (
+        <div
+            className={clickedValue && info ? 'overlay-movie show' : 'overlay-movie'}
+            onClick={() => setClickedValue(false)}
+        >
+            <i onClick={() => setClickedValue(false)} className='fas fa-window-close'></i>
+            <h2>{title}</h2>
+            <h3>{date}</h3>
+            <p>{overview}</p>
+        </div>
+    );
+}
